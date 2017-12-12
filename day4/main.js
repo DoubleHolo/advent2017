8efb2cb5ed1777d3ca2844425aa26a77fc643dca
Brian Cali <doubleholo@gmail.com>
2017-12-11 21:22:58 -0500

    Day 4

---
+++ day4/main.js
@@ -0,0 +1,62 @@
+fs = require('fs')
+fs.readFile('./day4/input.txt', 'utf8', function (err,data) {
+    if (err) {
+
+        return console.log(err);
+    }
+    processFile(data);
+});
+
+function parseLines(data)
+{
+    return data.split("\n");
+}
+
+function processFile(data)
+{
+    var valid = 0;
+    var lines = parseLines(data);
+    for(var i = 0; i < lines.length; i++)
+    {
+        var success = validateLine(lines[i]);
+        if(success)
+        {
+            valid++;
+        }
+    }
+
+    console.log("Valid Count: " + valid);
+}
+
+function validateLine(line)
+{
+    var valid = true;
+    var words = line.split(/\s+/);
+    for(var i = 0; i< words.length; i++)
+    {
+
+        for(var k = 0; k < words.length; k++)
+        {
+            if(k != i)
+            {
+                if(orderWord(words[i]) == orderWord(words[k]))
+                {
+                    valid = false;
+                }
+
+            }
+        }
+    }
+    return valid;
+}
+
+function orderWord(word)
+{
+    var orderedWord = "";
+
+    var chars = word.split('');
+    chars.sort();
+    orderedWord = chars.join("");
+
+    return orderedWord;
+}
